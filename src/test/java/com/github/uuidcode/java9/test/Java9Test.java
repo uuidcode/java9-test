4fac3de4508cf4e68a9d62a6cd52091b27468086
uuidcode <uuidcode@gmail.com>
2017-09-27 18:49:08 +0900

    processHandler

--- src/test/java/com/github/uuidcode/java9/test/Java9Test.java
+++ src/test/java/com/github/uuidcode/java9/test/Java9Test.java
@@ -3,6 +3,9 @@ package com.github.uuidcode.java9.test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -31,5 +34,31 @@ public class Java9Test {
         }
 
         assertThat(content).isNotNull();
+    }
+
+    @Test
+    public void processHandler() {
+        ProcessHandle.allProcesses()
+            .filter(p -> p.info().command().orElse("").contains("java"))
+            .forEach(p -> {
+                if (logger.isDebugEnabled()) {
+                    logger.debug(">>> pid: {}", p.pid());
+                    logger.debug(">>> command: {}", p.info().command());
+                    logger.debug(">>> startInstant: {}", p.info().startInstant());
+                }
+            });
+    }
+
+    @Test
+    public void collectionFactoryMethod() {
+        List<String> immutableList = List.of("one", "two");
+        Set<String> immutableSet = Set.of("one", "two");
+        Map<String, String> immutableMap = Map.of("one", "1", "two", "2");
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(">>> immutableMap: {}", immutableMap);
+        }
+
+        assertThat(immutableMap.size()).isGreaterThan(0);
     }
 }
