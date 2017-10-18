3b05d793410b2c140eda9e77e8788d87295eb73a
uuidcode <uuidcode@gmail.com>
2017-10-18 11:00:42 +0900

    compile.sh

---
+++ module-path/bin/compile.sh
@@ -0,0 +1,8 @@
+javac -classpath \
+../lib/commons-lang3-3.6.jar \
+--module-path \
+../mod \
+-d \
+../out \
+../src/main/java/com/github/uuidcode/mp/HelloModulePath.java \
+../src/main/java/module-info.java
